a36ad196ee4c3a8723c3aec0be067c57386c7407
Chen Shu <csw.dawn@gmail.com>
2020-01-05 16:30:59 +0800

    update sql file

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,10 +1,11 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"gin-atomy/server/service/products"
+	"github.com/gin-gonic/gin"
 )
 
+// Engine - engine
 func Engine() *gin.Engine {
 	r := gin.Default()
 
